7e9e8179d1a09c3f598469fc1ad60d2e846fd510
yanglu <1258947325@qq.com>
2022-10-28 18:10:50 +0800

    改变结构

--- src/platform/kuaiShou/index.js
+++ src/platform/kuaiShou/index.js
@@ -1,7 +1,9 @@
 const renderPlugin = require('../common/renderPlugin')
+const pageFn = require('./pageFn')
 const Api = require('./api')
 const App = {
   renderPlugin,
-  Api
+  Api,
+  pageFn
 }
 module.exports = App
---
+++ src/platform/kuaiShou/pageFn.js
@@ -0,0 +1,4 @@
+function pageFn () {
+  console.log('获取数据并出发渲染')
+}
+module.exports = pageFn
