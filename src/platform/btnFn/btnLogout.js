4a098c5dd8fd1fd0b1ed14b17ba4c4a82b721073
yanglu <1258947325@qq.com>
2022-11-09 18:13:44 +0800

    退出刷新

--- src/platform/btnFn/btnLogout.js
+++ src/platform/btnFn/btnLogout.js
@@ -1,9 +1,11 @@
 const ChromeStorage = require('@/utils/ChromeStorage')
+const Util = require('@/utils/Util')
 function btnLogout () {
   $('#btn-wrapper .btn-logout').click(function () {
     ChromeStorage.remove('token').then(() => {
-      $('#previewWrap').hide()
-      $('#loginWrap').show()
+      // $('#previewWrap').hide()
+      // $('#loginWrap').show()
+      Util.refresh()
     })
   })
 }
