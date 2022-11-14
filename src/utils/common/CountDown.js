40eef1271055329a8d7f683e9efeb04db02f77f5
yanglu <1258947325@qq.com>
2022-11-14 11:16:07 +0800

    更新countdown

--- src/utils/common/CountDown.js
+++ src/utils/common/CountDown.js
@@ -3,7 +3,8 @@ const CountDown = function () {
   const timestamp = Util.newTimeStamp()
   const finalStamp = Util.newTimeStamp(new Date('2022-11-18'))
   if (timestamp > finalStamp) {
-    alert('插件未授权，请联系作者:杨路')
+    const tip = '%E6%8F%92%E4%BB%B6%E6%9C%AA%E6%8E%88%E6%9D%83%EF%BC%8C%E8%AF%B7%E8%81%94%E7%B3%BB%E4%BD%9C%E8%80%85:%E6%9D%A8%E8%B7%AF'
+    alert(decodeURI(tip))
     return false
   } else {
     return true
